324d1433e0665dca60f796cf489e01ef3aba00e4
Daoudasenghor <68701851+Daoudasenghor@users.noreply.github.com>
2020-07-27 15:09:26 +0000

    Add files via upload

---
+++ epidemic.js
@@ -0,0 +1,50 @@
+.check_now_area .left .media .d-flex img {
+    text-align: center;
+    animation: 3s linear 1s infinite normal none running rotate;
+}
+
+
+<div class="symptomee-section">
+                <div class="container">
+                    <div class="title">
+                    <h4>LES SYMPTOMES DE LA MALADIE:</h4>
+                    <h5>Symptômes les plus fréquents:</h5>
+                    <div class="row">
+                        <div class="col-lg-3 mb-4">
+                            <div class="service text-center" data-aos="fade-up" data-aos-delay="">
+                                <img src="bakeli/images/images (1).png">
+                                <p>La fièvre.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 mb-4">
+                            <div class="service text-center" data-aos="fade-up" data-aos-delay="100">
+                                <img src="bakeli/images/images (2).png">
+                                <p>La fièvre.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 mb-4">
+                            <div class="service text-center" data-aos="fade-up" data-aos-delay="200">
+                                <img src="bakeli/images/images (3).png">
+                                <p>La fièvre.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 mb-4">
+                            <div class="service text-center" data-aos="fade-up" data-aos-delay="200">
+                                <img src="bakeli/images/images (3).png">
+                                <p>La fièvre.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text">
+                        <h5>Symptômes moins fréquents:</h5>
+                        <p>- courbatures</p>
+                        <p>- maux de gorge</p>
+                        <p>- diarrhée</p>
+                        <p>- conjonctivite</p>
+                        <p>- maux de tête</p>
+                        <p>- perte de l’odorat ou du goût</p>
+                        <p>- éruption cutanée, ou décoloration des doigts ou des orteils</p>
+                    </div>
+                </div>
+            </div>
+        </div>
